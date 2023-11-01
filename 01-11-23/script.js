6b6e06555d71d6504f980f65e516b050112b37a8
Simona Sunrise <148492087+SimonaSunrise@users.noreply.github.com>
2023-11-01 17:43:37 +0100

    Add files via upload

---
+++ 01-11-23/script.js
@@ -0,0 +1,14 @@
+const Animals = [
+  "Lepre",
+  "Corvo",
+  "Serpente",
+  "Coniglio",
+  "Leone",
+  "Orso",
+  "Volpe",
+  "Cane",
+];
+
+for (let i = 0; i < Animals.length; i++) {
+  console.log((i + 1).toString(), ":", Animals[i]);
+}
